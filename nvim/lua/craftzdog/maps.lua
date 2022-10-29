b2c172a0c5405c11c7b3109202d97194128ace1f
tonymarsh <tonymarsh0331@gmail.com>
2022-10-29 22:10:30 +0800

    添加了保存寄存器数据的paste操作映射

--- nvim/lua/craftzdog/maps.lua
+++ nvim/lua/craftzdog/maps.lua
@@ -35,3 +35,7 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- vim特性:如果copy的内容直接在已有数据上进行以替换为目的paste，那么寄存器中的内容就发生了变化，成为了被替换的内容
+-- 若想保留寄存器中的数据，进行多次数据替换，可以使用leader + p
+keymap.set('', '<leader>p', '\"_dp')
